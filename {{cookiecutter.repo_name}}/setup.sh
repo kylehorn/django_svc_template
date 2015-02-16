352e3b1ef582e4562acedb9e3b1344ebeefb65b6
Kyle Horn <kylehorn@me.com>
2015-02-16 03:57:00 -0600

    setup file

--- {{cookiecutter.repo_name}}/setup.sh
+++ {{cookiecutter.repo_name}}/setup.sh
@@ -8,20 +8,42 @@ if [[ ! -x `which pip` ]]; then
     sudo easy_install pip
 fi
 
+echo 'pip installed'
+
+
 # virtualenvwrapper is used to keep enviroments standard
 echo 'Installing virtualenvwrapper'
 
-if [[ -x `which pip` && ! -x `which virtualenvwrapper` ]]; then
+if [[ -x `which pip` && ! -x `which mkvirtualenv` ]]; then
     pip install virtualenvwrapper
     export WORKON_HOME=~/.envs
     mkdir -p $WORKON_HOME
     source /usr/local/bin/virtualenvwrapper.sh
-    mkvirtualenv {{ cookiecutter.repo_name }}
 fi
 
-# pip install is used to install requirements
-echo 'Installing requirements'
+echo 'virtualenvwrapper installed'
 
-if [[ -x `which pip` ]]; then
-    pip install -r requirements.txt
-fi
+
+# set up virtualenv
+echo 'Creating virtualenv for {{ cookiecutter.repo_name }}'
+
+mkvirtualenv {{ cookiecutter.repo_name }} -r requirements.txt -a .
+EOF
+
+echo 'Virtualenv for {{ cookiecutter.repo_name }} created'
+
+# move settings file
+echo 'Creating local settings file'
+mv {{cookiecutter.repo_name}}/local_settings_template.py {{cookiecutter.repo_name}}/local_settings.py
+echo 'Local settings file created'
+
+echo 'Collect static files'
+python manage.py collectstatic --noinput
+echo 'Static files collected'
+
+echo 'Create database'
+python manage.py migrate
+echo 'Database created'
+
+echo 'Start server'
+python manage.py runserver_plus
